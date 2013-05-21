e32a8af15ea8b19d8e491010fb644e9cbfe67e58
clem <>
2013-05-21 15:20:18 +0200

    HGR\UserBundle et suppression Acme

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,11 +20,10 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new Acme\UserBundle\AcmeUserBundle(),
+            new HGR\UserBundle\HGRUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
