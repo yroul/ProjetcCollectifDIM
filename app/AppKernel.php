00db590e804f4d2bd1d3dceeceb196e4c4ad1cc2
yroul <yoan.roullard@gmail.com>
2013-05-21 16:56:15 +0200

    Revert "Merge branch 'origin/UserBundle'"
    This reverts commit 82c29c46b20f3884eada426710957f59f41088cb, reversing
    changes made to a7b6662795360f1fa543710c6db15a9a1afba60a.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,12 +19,12 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new HGR\UserBundle\HGRUserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new HGR\EntityBundle\HGREntityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+           // $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
