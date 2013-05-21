d006bdc157ac0a9d6d4923aa896a5d07e645d28f
yroul <yoan.roullard@gmail.com>
2013-05-21 11:40:52 +0200

    Add mark entity

---
+++ src/Acme/DemoBundle/Entity/Mark.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Acme\DemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Mark
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Mark
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="Value", type="float")
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param float $value
+     * @return Mark
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return float 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
