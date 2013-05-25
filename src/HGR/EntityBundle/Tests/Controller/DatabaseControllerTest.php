1c679bc43df2ed806ca3ad3559132e1a05598f9e
yroul <yoan.roullard@gmail.com>
2013-05-25 13:05:51 +0200

    Add create /read simple test for product

---
+++ src/HGR/EntityBundle/Tests/Controller/DatabaseControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace HGR\EntityBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use HGR\EntityBundle\Entity\Product as Product;
+use \InvalidArgumentException as InvalidArgumentException;
+class DatabaseControllerTest extends WebTestCase
+{
+    public function setUp() {
+        
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $this->em = $kernel->getContainer()
+                             ->get('doctrine.orm.entity_manager');
+        $this->repo = $kernel->getContainer()
+                             ->get('doctrine.orm.entity_manager')
+                             ->getRepository("HGREntityBundle:Product");
+      
+    }
+    public function tearDown() {
+        //on tearDownm, only delete product prefixed by "test_"
+        //to avoid side effect on others stored objects
+        parent::tearDown();
+        $products = $this->repo->findAll();
+        $em = $this->em;
+        foreach ($products as $key => $value) {            
+            if(substr($products[$key]->getName(),0,4) == "test")
+                $em->remove($products[$key]);
+        }
+        $em->flush();
+        
+    }
+    
+    public function testProductCreateReadSimpleProduct(){
+        
+        $product = new Product();
+        $product->setName("test_monProduit");
+        $em = $this->em;
+        $em->persist($product);
+        $em->flush();
+        $productFetched = $this->repo->find($product->getId());
+        $this->assertEquals($productFetched->getName(),"test_monProduit");
+        
+              
+    }
+    
+}
