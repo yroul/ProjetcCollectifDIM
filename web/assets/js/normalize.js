617baf473ea48d7856e7d2f69ac86d78b60f10e4
clem <>
2013-06-04 16:08:48 +0200

    Finalisation css 	User, Gestion stock, images upload et icons

--- web/assets/js/normalize.js
+++ web/assets/js/normalize.js
@@ -3,15 +3,37 @@ $(document).ready(function(){
    /*
     * Forms, fields and buttons
     */
-   $(".container").find("> form").addClass("form-horizontal");
-   $(".form-horizontal").find("label").addClass("control-label");
-   $(".form-horizontal").find("div > div").addClass("control-group");
-   $(".form-horizontal").find("input").wrap("<div class='controls' />");
-   $(".form-horizontal").find("select").wrap("<div class='controls' />");
+   $(".container").find("> form")
+                  .addClass("form-horizontal");
+                  
+   $(".form-horizontal").find("fieldset div > div:not(#hgr_userbundle_usertype_roles)")
+                        .addClass("control-group");
+                        
+   $(".form-horizontal").find("input:not([type='checkbox'])")
+                        .wrap("<div class='controls' />");
+                        
+   $(".form-horizontal").find("select")
+                        .wrap("<div class='controls' />");
+                        
+   $(".control-group").find("> label:not(.checkbox)")
+                      .addClass("control-label");
    
-   $(".form-horizontal").find("p").find("button").parent().addClass("form-actions");
-   $("form").find("button[type='submit']").addClass("btn-primary");
-   $("form").find("button").addClass("btn");
+   $(".form-horizontal").find("p")
+                        .find("button")
+                        .parent()
+                        .addClass("form-actions");
+                        
+   $("form").find("button[type='submit']")
+            .addClass("btn-primary");
+            
+   $("form").find("button")
+            .addClass("btn");
+            
+   $(".form-horizontal").find("input[type='checkbox']")
+                        .css("margin-left", "23px")
+                        .parent()
+                        .find("label")
+                        .addClass("checkbox inline");
    
    /*
     * Images
