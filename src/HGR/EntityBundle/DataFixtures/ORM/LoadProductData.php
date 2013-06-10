b84bf0cff1bfeca95dc4534decda34f8eeb02f5e
yroul <yoan.roullard@gmail.com>
2013-06-10 11:44:51 +0200

    Improve Fixtures

--- src/HGR/EntityBundle/DataFixtures/ORM/LoadProductData.php
+++ src/HGR/EntityBundle/DataFixtures/ORM/LoadProductData.php
@@ -10,6 +10,7 @@ use HGR\EntityBundle\Entity\Store;
 use HGR\EntityBundle\Entity\StockProduct;
 use HGR\EntityBundle\Entity\Mark;
 use HGR\EntityBundle\Entity\Comment;
+use HGR\UserBundle\Entity\User;
 class LoadProductData implements FixtureInterface
 {
     /**
@@ -19,36 +20,53 @@ class LoadProductData implements FixtureInterface
     public function load(ObjectManager $manager) {
         //create a ctagory
         $category = new Category();
-        $category->setName("Test_categorie1");
+        $category->setName("Bieres");
         
         //create somme product and add category to each
         $product1 = new Product();
-        $product1->setName("Test_produit1");
-        $product1->setPrice(15.5);
+        $product1->setName("Bière d'alpage");
+        $product1->setPrice(4.5);
         $product1->setCategory($category);
-        $product1->setBrand("marque1");
-        $product1->setTags("tag,tag2");
+        $product1->setBrand("Mon Alpage");
+        $product1->setTags("biere,alpage,bon");
         $product2 = new Product();
-        $product2->setName("Test_produit2");
+        $product2->setName("La Dexter");
         $product2->setPrice(8.99);
         $product2->setCategory($category);
-        $product2->setBrand("marque1");
+        $product2->setBrand("ACME Company");
         $product2->setTags("tag,tag2");
         $product3 = new Product();
-        $product3->setName("Test_produit3");
-        $product3->setPrice(0.5);
+        $product3->setName("Duff");
+        $product3->setPrice(2.6);
         $product3->addMark(new Mark(4.5));
         $product3->setCategory($category);
-        $product3->setBrand("marque1");
+        $product3->setBrand("Duff Corp");
         $product3->setTags("tag,tag2");
+        $product4 = new Product();
+        $product4->setName("Bières des trolls");
+        $product4->setCategory($category);
+        $product4->setBrand("BelgiumSAS");
+        $product4->setPrice(10);
         //persist product (and category)
         $manager->persist($product1);
         $manager->persist($product2);
         $manager->persist($product3);
+        $manager->persist($product4);
         
+        
+        //user
+        //$userManager = $this->get('fos_user.user_manager');
+        $user = new User();
+        $user->setPlainPassword("password");
+        $user->setUsername("admin");
+        $user->setRoles(array("ROLE_ADMIN"));
+        $user->setEnabled(true);
+        $user->setEmail("admin@example.com");
         //create store
         $store1 = new Store();
-        $store1->setName("magasin1");
+        $store1->setName("Magasin Annecy");
+        $store2 = new Store();
+        $store2->setName("Magasin Lyon");
         //create stock product
         $stockStore1 = new StockProduct();
         //add 15 product1 to this stock
@@ -56,7 +74,7 @@ class LoadProductData implements FixtureInterface
         $stockStore1->setAmount(15);
         $store1->addStocks($stockStore1);
         $manager->persist($store1);
-        
+        $manager->persist($user);
         $manager->flush();
     }    
 }
